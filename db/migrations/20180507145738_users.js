dd018a57a275600798d36fafeb58dd0c662db7d7
Abe Queen <abedababe8@gmail.com>
2018-05-07 15:04:33 -0700

    added migrations

---
+++ db/migrations/20180507145738_users.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('first_name').notNullable().defaultTo('');
+    table.string('last_name').notNullable().defaultTo('');
+    table.string('email').notNullable().defaultTo('');
+    table.string('hashed_password').notNullable().defaultTo('');
+    table.boolean('admin').notNullable().defaultTo(false)
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users')
+};
