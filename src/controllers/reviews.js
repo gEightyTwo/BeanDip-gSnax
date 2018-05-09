9db033e80fb6b693fdeada530fe557d0c3aa6573
Abe Queen <abedababe8@gmail.com>
2018-05-09 13:23:59 -0700

    weird glitch

--- src/controllers/reviews.js
+++ src/controllers/reviews.js
@@ -1,19 +1,44 @@
-const userModel = require('../models/reviews')
+const reviewModel = require('../models/reviews')
 
-function getOne(req, res, next){
-
-}
 
 function getAll(req, res, next){
+  reviewModel.getAll()
+  .then(allReviews => {
+    res.status(200).send({ allReviews })
+  })
+}
 
+function getOne(req, res, next){
+  if(!req.params.reviewsId){
+    return next({status:400, message:'Bad Request'})
+  }
+  reviewModel.getOne(req.params.reviewsId)
+  .then(review => {
+    res.status(200).send({review})
+  })
+  .catch(next)
 }
 
 function create(req, res, next){
-
+  if(!req.params.usersId){
+    return next({status:400, message:'Bad Request'})
+  }
+  reviewModel.create(req.params.usersId,req.params.snackId, req.body.title, req.body.text, req.body.rating)
+  .then(review => {
+    res.status(200).send({review})
+  })
+  .catch(next)
 }
 
 function update(req, res, next){
-
+  if(!req.params.usersId || !req.params.reviewsId){
+    return next({status:400, message:'Bad Request'})
+  }
+  reviewModel.update(req.params.usersId, req.params.reviewsId, req.body.title, req.body.text, req.body.rating)
+  .then(review => {
+    res.status(200).send({review})
+  })
+  .catch(next)
 }
 
 function remove(req, res, next){
---
+++ src/models/reviews.js
@@ -0,0 +1,32 @@
+const knex = require('../../db/knex');
+const bcrypt = require('bcrypt-as-promised')
+
+function getAll(){
+  return (
+    db('reviews')
+    .returning('*')
+  )
+}
+
+function getOne(id){
+  return (
+    db('reviews')
+    .where({id})
+  )
+}
+
+function create(usersID, snackId, title, text, rating){
+  return (
+    db('reviews')
+    .insert({user_id: usersID, snack_id: snackId, title, text, rating})
+    .returning('*')
+  )
+}
+
+function update(usersID, reviewsId, title, text, rating){
+  return (
+    db('reviews')
+    .where({})
+    .insert({})
+  )
+}
