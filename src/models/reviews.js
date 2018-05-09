b230b8492149f2e12fd670b90163439caddd5ef7
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-05-09 13:52:16 -0700

    up to date

---
+++ src/models/reviews.js
@@ -0,0 +1,25 @@
+const knex = require('../../db/knex');
+const bcrypt = require('bcrypt-as-promised')
+
+function getOne(){
+
+}
+
+function getAll(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
+
+function remove(){
+
+}
+
+
+module.exports = {getOne, getAll, create, update, remove)
